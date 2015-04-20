45559b60a4cc72f606f0e042c64dcd09957298c3
Sayaka <s13h108@ya.osaka-sandai.ac.jp>
2015-04-21 01:31:59 +0900

    test2

---
+++ Assets/BallContoller.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+var Speed = 20.0;
+function Start () {
+		rigidbody.AddForce(
+		( transform.forward + transform.right ) * Speed,
+			ForceMode.VelocityChange );
+}
+
+function Update () {
+
+}
+function OnCollisionEnter()
+{
+    rigidbody.velocity = rigidbody.velocity.normalized * 15;
+
+    if (Mathf.Abs(rigidbody.velocity.z) < 3) {
+        rigidbody.velocity.z = rigidbody.velocity.z * 3;
+    }
+    if (Mathf.Abs(rigidbody.velocity.x) < 3) {
+        rigidbody.velocity.x = rigidbody.velocity.x * 3;
+    }
+
+
+}
